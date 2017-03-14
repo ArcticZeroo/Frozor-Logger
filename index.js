979b118d3de6fb8d7bf89a22fd0540c77948bf12
Spencer <spencer@artix.cc>
2017-03-13 22:33:02 -0400

    Finally fix command with a stupid hack

--- index.js
+++ index.js
@@ -6,21 +6,29 @@ const colors = {
     INFO: chalk.green,
     ERROR: chalk.red,
     DEBUG: chalk.yellow,
-    WARN: chalk.magenta
+    WARN: chalk.magenta,
+    COMMAND: chalk.cyan
 };
 
 const getColor = (type)=> colors[type.toUpperCase()] || chalk.white;
+const getTimestamp = ()=> dateFormat(new Date(), 'mm/dd/yyyy HH:MM:ss');
+const messageFormatter = (opt, prefix)=> `${chalk.white(`[${opt.timestamp()}]`)} ${getColor(opt.level)(`${(opt.meta.prefix)?`[${(prefix)?`${prefix}|${opt.meta.prefix}`:opt.meta.prefix}] `:(prefix)?`[${prefix}] ` : ''}`)}${getColor(opt.level).bold(`${opt.level.toUpperCase()}`)}${chalk.white(`: ${opt.message}`)}`;
 
 class Logger extends winston.Logger{
     constructor(prefix, filename){
         super({
             colorize: false,
             transports:[
-                new (winston.transports.File)({ filename: (filename||prefix||'frozor')+'-logger.log', levels: { error: 0, warn: 1, command: 2, info: 3, verbose: 4, debug: 5, silly: 6 } }),
+                new (winston.transports.File)({ filename: (filename||prefix||'frozor')+'-logger.log'}),
                 new (winston.transports.Console)({
                     levels: { error: 0, warn: 1, command: 2, info: 3, verbose: 4, debug: 5, silly: 6 },
-                    timestamp: ()=> dateFormat(new Date(), 'mm/dd/yyyy HH:MM:ss'),
-                    formatter: (opt)=> `${chalk.white(`[${opt.timestamp()}]`)} ${getColor(opt.level)(`${(opt.meta.prefix)?`[${(prefix)?`${prefix}|${opt.meta.prefix}`:opt.meta.prefix}] `:(prefix)?`[${prefix}] ` : ''}`)}${getColor(opt.level).bold(`${opt.level.toUpperCase()}`)}${chalk.white(`: ${opt.message}`)}`
+                    timestamp: getTimestamp,
+                    formatter: (opt)=>{
+                        if(opt.meta.overrideLevel){
+                            opt.level = opt.meta.overrideLevel;
+                        }
+                        return messageFormatter(opt, prefix);
+                    }
                 })
             ]
         });
@@ -32,14 +40,17 @@ class Logger extends winston.Logger{
         this.chalk = chalk;
     }
 
-    log(level, message, prefix){
-        super.log(level, message, {
+    log(level, message, prefix, meta = {}){
+        super.log(level, message, Object.assign({
             prefix: prefix
-        });
+        }, meta));
     }
 
     command(name, cmd, type, success){
-        this.log('command', `${chalk.cyan(name)} executed ${chalk.cyan(type)} command ${chalk.magenta(cmd)} ${(success)?chalk.green('Successful'):chalk.red('Unsuccessfully')}`);
+        super.log('info', `${chalk.cyan(name)} executed ${chalk.cyan(type)} command ${chalk.magenta(cmd)} ${(success)?chalk.green('Successfuly'):chalk.red('Unsuccessfully')}`, {
+            overrideLevel: 'command'
+        });
+        return this;
     }
 }
 
