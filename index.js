59f73f20090a4519d494ed2aeb697fa558fd9d4e
Spencer <spencer@artix.cc>
2017-03-15 19:03:12 -0400

    Add dynamic log levels if env is dev

--- index.js
+++ index.js
@@ -14,13 +14,14 @@ const colors = {
 const getColor = (type)=> colors[type.toUpperCase()] || chalk.white;
 const getTimestamp = ()=> dateFormat(new Date(), 'mm/dd/yyyy HH:MM:ss');
 const messageFormatter = (opt, prefix)=> `${chalk.white(`[${opt.timestamp()}]`)} ${getColor(opt.level)(`${(opt.meta.prefix)?`[${(prefix)?`${prefix}|${opt.meta.prefix}`:opt.meta.prefix}] `:(prefix)?`[${prefix}] ` : ''}`)}${getColor(opt.level).bold(`${opt.level.toUpperCase()}`)}${chalk.white(`: ${opt.message}`)}`;
+const getLevel = ()=> process.env.NODE_ENV == 'dev' ? 'debug' : 'info';
 
 class Logger extends winston.Logger{
     constructor(prefix, filename){
         super({
             colorize: false,
             transports:[
-                new (winston.transports.File)({ filename: 'logs/'+(filename||prefix||'frozor')+'-logger.log', formatter: (opt)=> chalk.stripColor(opt.message)}),
+                new (winston.transports.File)({ filename: 'logs/'+(filename||prefix||'frozor')+'-logger.log', formatter: (opt)=> chalk.stripColor(opt.message), level: getLevel()}),
                 new (winston.transports.Console)({
                     levels: { error: 0, warn: 1, command: 2, info: 3, verbose: 4, debug: 5, silly: 6 },
                     timestamp: getTimestamp,
@@ -29,7 +30,8 @@ class Logger extends winston.Logger{
                             opt.level = opt.meta.overrideLevel;
                         }
                         return messageFormatter(opt, prefix);
-                    }
+                    },
+                    level: getLevel()
                 })
             ]
         });
