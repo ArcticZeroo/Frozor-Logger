536982d3f3e2b4546f09980c57068412f8d0157a
Spencer <spencer@artix.cc>
2017-08-16 16:20:08 -0400

    Add docs, cry to jesus, reformat stuff

--- index.js
+++ index.js
@@ -8,20 +8,29 @@ const colors = {
     ERROR: chalk.red,
     DEBUG: chalk.yellow,
     WARN: chalk.magenta,
-    COMMAND: chalk.cyan
+    COMMAND: chalk.cyan,
+    get: c => colors[c.toUpperCase()] || chalk.white
 };
 
-const getColor = (type)=> colors[type.toUpperCase()] || chalk.white;
-const getTimestamp = ()=> dateFormat(new Date(), 'mm/dd/yyyy HH:MM:ss');
-const messageFormatter = (opt, prefix)=> `${chalk.white(`[${opt.timestamp()}]`)} ${getColor(opt.level)(`${(opt.meta.prefix)?`[${(prefix)?`${prefix}|${opt.meta.prefix}`:opt.meta.prefix}] `:(prefix)?`[${prefix}] ` : ''}`)}${getColor(opt.level).bold(`${opt.level.toUpperCase()}`)}${chalk.white(`: ${opt.message}`)}`;
-const getLevel = ()=> process.env.NODE_ENV == 'dev' ? 'debug' : 'info';
+function getTimestamp () {
+    return dateFormat(new Date(), 'mm/dd/yyyy HH:MM:ss');
+}
+
+// jesus have mercy
+const messageFormatter = (opt, prefix)=> `${chalk.white(`[${opt.timestamp()}]`)} ${colors.get(opt.level)(`${(opt.meta.prefix)?`[${(prefix)?`${prefix}|${opt.meta.prefix}`:opt.meta.prefix}] `:(prefix)?`[${prefix}] ` : ''}`)}${colors.get(opt.level).bold(`${opt.level.toUpperCase()}`)}${chalk.white(`: ${opt.message}`)}`;
+
+function getLogLevel() {
+    return process.env.NODE_ENV === 'dev' ? 'debug' : 'info';
+}
+
+const LOG_LEVEL = process.env.NODE_ENV === 'dev' ? 'debug' : 'info';
 
 class Logger extends winston.Logger{
     constructor(prefix, filename){
         super({
             colorize: false,
             transports:[
-                new (winston.transports.File)({ filename: 'logs/'+(filename||prefix||'frozor')+'-logger.log', formatter: (opt)=> chalk.stripColor(opt.message), level: getLevel()}),
+                new (winston.transports.File)({ filename: 'logs/'+(filename||prefix||'frozor')+'-logger.log', formatter: (opt)=> chalk.stripColor(opt.message), level: LOG_LEVEL}),
                 new (winston.transports.Console)({
                     levels: { error: 0, warn: 1, command: 2, info: 3, verbose: 4, debug: 5, silly: 6 },
                     timestamp: getTimestamp,
@@ -31,7 +40,7 @@ class Logger extends winston.Logger{
                         }
                         return messageFormatter(opt, prefix);
                     },
-                    level: getLevel()
+                    level: LOG_LEVEL
                 })
             ]
         });
@@ -46,21 +55,68 @@ class Logger extends winston.Logger{
 
         this.warning = this.warn;
 
+        /**
+         * Literally the npm module chalk.
+         * @type {object}
+         */
         this.chalk = chalk;
+
+        /**
+         * @name Logger#info
+         * @function
+         * @memberOf Logger
+         * @description Log a message with the 'INFO' prefix, and log level 'info'
+         * @param {string} message - The message to log
+         * @param {string} [prefix] - The prefix to include in the log
+         */
+        /**
+         * @name Logger#error
+         * @function
+         * @memberOf Logger
+         * @description Log a message with the 'ERROR' prefix, and log level 'error'
+         * @param {string} message - The message to log
+         * @param {string} [prefix] - The prefix to include in the log
+         */
+        /**
+         * @name Logger#warn
+         * @function
+         * @memberOf Logger
+         * @description Log a message with the 'WARN' prefix, and log level 'warn'
+         * @param {string} message - The message to log
+         * @param {string} [prefix] - The prefix to include in the log
+         */
+        /**
+         * @name Logger#debug
+         * @function
+         * @memberOf Logger
+         * @description Log a message with the 'DEBUG' prefix, and log level 'debug'. Set NODE_ENV=dev to see these without setting levels.
+         * @param {string} message - The message to log
+         * @param {string} [prefix] - The prefix to include in the log
+         */
+        /**
+         * @name Logger#command
+         * @function
+         * @memberOf Logger
+         * @description Log a command's use to the console.
+         * @param {string} name - The name of the user that executed the command.
+         * @param {string} cmd - The name of the command that was executed (or the full text, if you want)
+         * @param {string} type - The command type. For instance, slack bots may want to use "Slack" as the type.
+         * @param {boolean} success
+         */
     }
 
     log(level, message, prefix, meta = {}){
-        super.log(level, message, Object.assign({
-            prefix: prefix
-        }, meta));
+        super.log(level, message, Object.assign({ prefix }, meta));
     }
 
-    command(name, cmd, type, success){
-        super.log('info', `${chalk.cyan(name)} executed ${chalk.cyan(type)} command ${chalk.magenta(cmd)} ${(success)?chalk.green('Successfuly'):chalk.red('Unsuccessfully')}`, {
+    command(name, cmd, type, success = true){
+        super.log('info', `${chalk.cyan(name)} executed ${chalk.cyan(type)} command ${chalk.magenta(cmd)} ${(success) ? chalk.green('Successfuly') : chalk.red('Unsuccessfully')}`, {
             overrideLevel: 'command'
         });
         return this;
     }
 }
+
+Logger.chalk = chalk;
 
 module.exports = Logger;
